4917354dc16093c3907911e1fdae2377bf41f4bb
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2020-03-31 14:38:46 -0700

    mlike

---
+++ src/components/common/like.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class Like extends Component {
+  render() {
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+    return (
+      <i
+        onClick={this.props.onClick}
+        className={classes}
+        style={{ cursor: "pointer" }}
+        aria-hidden="true"
+      ></i>
+    );
+  }
+}
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-
+import Like from "./common/like";
 class Movies extends Component {
   state = {
     movies: getMovies()
@@ -9,6 +9,13 @@ class Movies extends Component {
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies: movies });
+  };
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 
   render() {
@@ -21,6 +28,7 @@ class Movies extends Component {
             <th>stock</th>
             <th>rate</th>
             <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -30,6 +38,13 @@ class Movies extends Component {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                {" "}
+                <Like
+                  onClick={() => this.handleLike(movie)}
+                  liked={movie.liked}
+                />
+              </td>
               <td>
                 <button
                   onClick={() => this.handleDelete(movie)}
